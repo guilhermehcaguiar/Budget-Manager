6c6f7bc56dcef54d92bf770b8c26950c4b6881c5
Guilherme Aguiar <guilherme.ghca@hotmail.com>
2026-03-02 23:15:51 -0300

    Initial commit: Setup do projeto Budget-Manager com H2 e estrutura de pastas

---
+++ src/test/java/com/guilhermehcaguiar/finance/budget_manager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermehcaguiar.finance.budget_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
